cf88c87950c34f24290ca43ca583cc445292f974
luis <luis.9993@gmail.com>
2015-12-22 13:30:59 -0300

    Subida de imagenes con parametro

--- Think-INK/src/main/java/service/FileUpload.java
+++ Think-INK/src/main/java/service/FileUpload.java
@@ -1,13 +1,20 @@
 package service;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParserFactory;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
@@ -17,6 +24,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import sun.misc.IOUtils;
 
 @Path("/")
 public class FileUpload {
@@ -33,11 +42,13 @@ public class FileUpload {
             for (Part part : request.getParts()) {
                 String name = null;
                 long size = 0;
+                
                 try {
                 	//Si es una imagen
-                	if(part.getContentType().toLowerCase()
+                	if(part.getContentType() != null && (
+                			part.getContentType().toLowerCase()
                             .startsWith("multipart/") || part.getContentType().toLowerCase()
-                            .startsWith("image")){
+                            .startsWith("image"))){
                 		InputStream inputStream = part.getInputStream();
                 		//Se selecciona el directorio donde se guardan las imagenes
                 		File f = new File(System.getProperty("user.home")+"/ImagenesServer/");
@@ -46,7 +57,7 @@ public class FileUpload {
                 		//Archivo de salida (Falta cambiar nombre e ingresar a base de datos)
                 		f = new File(System.getProperty("user.home")+"/ImagenesServer/imagenn.jpg");
                         FileOutputStream outputStream = new FileOutputStream(f);
-
+                    	
     	        		int read = 0;
     	        		byte[] bytes = new byte[1024];
     	
@@ -54,27 +65,40 @@ public class FileUpload {
     	        			outputStream.write(bytes, 0, read);
     	        		}
     	        		outputStream.close();
-            		//Si es un dato adicional
-                	}else if(part.getContentType() != null){
+
                 		name = getSubmittedFileName(part);
                         size = part.getSize();
-                        
+                        //part.delete();
+            		//Si es un dato adicional
+                	}else{
+                		//Leyendo valor
+                		BufferedReader streamReader = new BufferedReader(new InputStreamReader(part.getInputStream(), "UTF-8")); 
+                		StringBuilder responseStrBuilder = new StringBuilder();
+            			
+                		String inputStr;
+                		while ((inputStr = streamReader.readLine()) != null)
+                		    responseStrBuilder.append(inputStr);
+                		//nombre valor
+                		String nombreVar = part.getName();
+                		String value = responseStrBuilder.toString();
+            			//Para postman
+                        array.add(Json.createObjectBuilder().add("name", nombreVar).add("value", value));
     	        		
-                        array.add(addFile(name, size, "anId"));
                         part.delete();
                 	}
                 	
                 //En caso de excepcion devuelve un json con el mensaje
                 } catch (Exception e) {
-                	
-                    array.add(addError(e.getMessage(), 312, "ERROR"));
+                	e.printStackTrace();
+                	array.add(Json.createObjectBuilder().add("name", e.getStackTrace().toString()));
+                    //array.add(addError(e.getMessage(), 312, "ERROR"));
                 }
             }
         } catch (IOException | ServletException e) {
             e.printStackTrace();
         }
 
-        JsonObject ret = Json.createObjectBuilder().add("files", array).build();
+        JsonObject ret = Json.createObjectBuilder().add("argumentos", array).build();
         return Response.status(201).entity(ret).build();
     }
     //Funcion auxiliar
@@ -87,14 +111,5 @@ public class FileUpload {
 	    }
 	    return null;
 	}
-    private JsonObjectBuilder addFile(String name, long size, String url) {
-        return Json.createObjectBuilder().add("name", name).add("size", size)
-                .add("lid", url);
-    }
-
-    private JsonObjectBuilder addError(String name, long size, String error) {
-        return Json.createObjectBuilder().add("name", name).add("size", size)
-                .add("error", error);
-    }
 
 }
